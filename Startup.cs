7887edf835a77e8bf685beb9c8176c21b5c15f00
Dariusz <darek.wantuch@wp.pl>
2020-12-21 22:03:14 +0100

    Rejestracja

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace WaterApplication
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
